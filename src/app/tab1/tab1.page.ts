0119714e4903a6ae9fae3c97c48a4a11c46b9c62
infy404 <shrijankarki96@gmail.com>
2022-02-06 03:12:04 -0500

    Calculate working

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+
+  totalHours;
+  hourlyRate;
+
+
+  rgPay = 0;
+  ovtPay = 0
+  ttlPay = 0;
+  ttlTax = 0;
+  ntIncome = 0;
+
   constructor() {}
+
+
+  calculatePay(){
+    if(this.totalHours <= 40){
+      this.rgPay = this.totalHours * this.hourlyRate;
+      this.ovtPay = 0;
+    }
+    else{
+      this.rgPay = 40 * this.totalHours;
+      this.ovtPay = (this.totalHours - 40) * this.hourlyRate * 1.5;
+    }
+
+    this.ttlPay = this.rgPay + this.ovtPay;
+
+    this.ttlTax = this.ttlPay * 0.18;
+
+    this.ntIncome = this.ttlPay - this.ttlTax;
+  }
 
 }
